58367e5d44845b9b37287c2da1f064f070c5ba5e
ivan.salnikov <ivan.salnikov@matryoshka.com>
2024-12-20 08:09:50 +0300

    ASDASD

--- code4.js
+++ code4.js
@@ -685,6 +685,7 @@ for (var i = 0, k = 0, l = gdjs.ShopCode.GDShopTileObjects1.length;i<l;++i) {
 gdjs.ShopCode.GDShopTileObjects1.length = k;
 if (isConditionTrue_0) {
 {gdjs.evtTools.storage.writeStringInJSONFile("Shop", "Shop", gdjs.evtTools.network.variableStructureToJSON(runtimeScene.getGame().getVariables().getFromIndex(5)));
+}{gdjs.evtTools.storage.writeStringInJSONFile("Saves", "Saves", gdjs.evtTools.network.variableStructureToJSON(runtimeScene.getGame().getVariables().getFromIndex(1)));
 }}
 
 }
